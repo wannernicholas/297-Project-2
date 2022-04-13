c34409b88a71825319bda4630b1afea0722c738e
wannernicholas <71566742+wannernicholas@users.noreply.github.com>
2022-04-13 15:15:45 -0400

    Initial Commit

---
+++ src/components/Viewer.js
@@ -0,0 +1,19 @@
+// ./src/components/Viewer.js
+
+// Viewer segment container
+export default function Viewer({ svgData }) {
+    // Contains the SVG display structure for the application
+
+    // Returns a JSX component for the SVG display
+    return (
+        <>
+            <img
+                src={`data:image/svg+xml;utf8,${encodeURIComponent(svgData)}`}
+                alt="SVG currently being displayed"
+                style={{
+                    maxWidth: 420, // Stop the SVG from expanding
+                }}
+            />
+        </>
+    );
+}
